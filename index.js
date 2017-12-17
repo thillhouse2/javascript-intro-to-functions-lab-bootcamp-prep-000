253d724102ff3024e9fa4aeb38d44fb290444f93
Tim Hillhouse <thillhouse2@gmail.com>
2017-12-17 20:04:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   if string.toLowerCase()===string {
+    return "I can\'t hear you!"
   } else if string.to toUpperCase()===string {
     
   }
