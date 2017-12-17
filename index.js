adea0e200a768b81e5732cc3cf8d1b64bbb24fe2
Tim Hillhouse <thillhouse2@gmail.com>
2017-12-17 20:07:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ function sayHiToGrandma(string){
   if (string.toLowerCase() === string) {
     return "I can\'t hear you!";
   } else if (string.toUpperCase()===string) {
-    return "YES INDEED!"
-  } else {
-    
+    return "YES INDEED!";
+  } else if (string===""){
+    return "I love you, too.";
   }
   }
 }
